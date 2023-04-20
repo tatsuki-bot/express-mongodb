d631c30b931e58e8239896e0ea23568089c79298
osonoi <yosonoi@gmail.com>
2023-04-20 11:13:35 +0900

    first commit

---
+++ deleteOne.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require("mongodb");
+// 下のURIをご自分の値に変更してください
+const uri = "mongodb+srv://matsunami2:banana1208@test.lfwohhl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+const database = client.db('notes');
+const notes = database.collection('notes');
+// idが2のドキュメントを削除
+const note = await notes.deleteOne({ id: 2});
+console.log(note);
+// 最後にクロースする
+await client.close();
+}
+run();
---
+++ findOne.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require("mongodb");
+// 下のURIをご自分の値に変更してください
+const uri = "mongodb+srv://matsunami2:banana1208@test.lfwohhl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+const database = client.db('notes');
+const notes = database.collection('notes');
+// idが１のドキュメントを取得
+const query = { id: 1 };
+const note = await notes.findOne(query);
+console.log(note);
+// 最後にクロースする
+await client.close();
+}
+run();
