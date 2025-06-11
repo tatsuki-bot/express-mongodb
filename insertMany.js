31d7c7075fd832bf1ec73a2fe54d733fccbd4762
加藤樹希 <katoujumare@katoujumarenoMacBook-Air.local>
2025-06-12 07:50:54 +0900

    insertMany

--- insertMany.js
+++ insertMany.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 // 下のURIをご自分の値に変更してください
-const uri = "mongodb+srv://matsunami2:password@test.lfwohhl.mongodb.net/?retryWrites=true&w=majorityß";
+const uri =  "mongodb+srv://aiueokttt209:<db_tatsuki3405>@cluster0.h1voyoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";//matsunami2:password@test.lfwohhl.mongodb.net/?retryWrites=true&w=majorityß";
 const client = new MongoClient(uri);
 async function run() {
 const database = client.db('notes');
